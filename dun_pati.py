8ac9ec0c6866eb453315f17848ba97faf8e843af
Piotr Gurdek <pgurdek@gmail.com>
2016-11-17 12:54:46 +0100

    Update Inventory, remove file

--- dun_pati.py
+++ dun_pati.py
@@ -1,4 +1,7 @@
 import random
+from operator import attrgetter
+from termcolor import cprint
+import os
 
 
 class Stuff():
@@ -95,3 +98,41 @@ def totalweight(loot):
     total_amount = sum([item.weight*item.amount for item in loot])
     print('Total weight of items: %s\n' % total_amount)
     return total_amount
+
+def display_inventory(inventory):
+
+    os.system('clear')
+    if not inventory:
+        inventory=[]
+        max_board = 55
+    else:
+        max_key = max(inventory, key=attrgetter('name'))
+        max_board = len(max_key.name)+55
+
+    total_amount = 0
+    total_weight = 0
+    max_length_key = '12'
+    print('Your inventory')
+    cprint('='*max_board,'yellow')
+    print('{:>{length_value}} {:>{length_value}} {:>{length_value}} {:>{length_value}}'.format('Item Name',
+        'Weight','Amount','Total weight',length_value=max_length_key))
+
+    for i,object in enumerate(inventory,start=1):
+        name = object.name
+        weight = object.weight
+        amount = object.amount
+        total_weight_item = weight * amount
+        total_weight += total_weight_item
+        total_amount += amount
+        print('{}. {:>{length_value}} {:>{length_value}} {:>{length_value}} {:>{length_value}}'.format(i,name,
+            weight, amount,total_weight_item,length_value=max_length_key))
+    print('\n')
+    print('Total items amount {} Total items weight: {}'.format(total_amount,
+        total_weight))
+    cprint('=' * max_board, 'yellow')
+    command = input('Commands: Drop Items [d], switch to game l ')
+
+    if command == 'd':
+        drop_item(inventory)
+    else:
+        print('Go go play')
--- dungeon_noclass.py
+++ dungeon_noclass.py
@@ -4,7 +4,6 @@ from kamil import *
 import start_game
 from dun_pati import *
 from  termcolor import cprint
-from inventory import *
 
 def getch():
     """Reads one character without enter"""
@@ -127,6 +126,7 @@ def boss(board):
     return board
 
 def create_level(level, loot):
+    hide = 0
     print(level)
     print('\n\n\n\n\n\n\n\n')
     items_position = []
@@ -140,27 +140,33 @@ def create_level(level, loot):
     start_board = random_item(start_board, items_position)
     doors(23,80, start_board, level)
     print_board(start_board)
+
     game_board = start_board[:]
     if level == 3:
         game_board = boss(game_board) 
     while True:
         os.system('clear')
         print_board(game_board,level)
-        display_inventory(loot)
         user_move = getch()
         hero = move(user_move, hero, game_board, door_pass,loot, level)
         hero_position(hero, game_board)
+
         if hero in items_position:
             what = items_position.index(hero)
             found = find_object(what,loot)
             items_position.pop(what)
             loot = add_to_inventory(found, loot)
+
+        if user_move == "l":
+            display_inventory(loot)
+
         if user_move == "\\":
             exit()
 
 def main ():
     loot = [rope, onion, dagger]
     level = 1
+    display_on_off = False
     start_game.start()
     create_level(level, loot)
 
